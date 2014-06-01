24195fe904456dd877ddf960e1471d3fdece6d81
Sicher2008 <sicher2008@yandex.ru>
2014-06-01 09:18:59 +0000

    Update db.php

--- db.php
+++ db.php
@@ -2,7 +2,6 @@
     /* Подключение к серверу MySQL */
     //ini_set("display_errors", 0);
     //ini_set("error_reporting", E_NONE);
-    $mysqli = new mysqli('212.224.113.37', 'rio32ru', 'aI,Q!#*{#WB9', 'rio32ru');
     $mysqli->query('SET NAMES  utf8');
     if (mysqli_connect_errno()) {
        printf("Подключение к серверу MySQL невозможно. Код ошибки: %s\n", mysqli_connect_error());
