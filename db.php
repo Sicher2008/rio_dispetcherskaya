9bd997f7a31fe76bf2706eaef65dd2657660737e
artem <sicherov@gmail.com>
2014-06-01 13:20:19 +0400

    bd local

--- db.php
+++ db.php
@@ -2,6 +2,7 @@
     /* Подключение к серверу MySQL */
     //ini_set("display_errors", 0);
     //ini_set("error_reporting", E_NONE);
+    $mysqli = new mysqli('localhost', 'root', '', 'rio_dispetcherskaya');
     $mysqli->query('SET NAMES  utf8');
     if (mysqli_connect_errno()) {
        printf("Подключение к серверу MySQL невозможно. Код ошибки: %s\n", mysqli_connect_error());
